fc2dbfe93caba54b7f4ca7bdbc34f312333dec90
yuta20253 <magee.20253@gmail.com>
2025-05-20 01:19:30 +0900

    feat:検索機能用スコープ実装

--- rails/app/models/recipe.rb
+++ rails/app/models/recipe.rb
@@ -8,4 +8,16 @@ class Recipe < ApplicationRecord
   accepts_nested_attributes_for :ingredients
 
   enum difficulty: { easy: 0, medium: 1, hard: 2 }
+
+  scope :search_recipe_name, ->(title) {
+    return all if title.blank?
+    where('title LIKE ?', "%#{title}") }
+
+  scope :select_difficulty, ->(dif) {
+    return all if dif.blank?
+    where(difficulty: dif) }
+
+  scope :registration_month, ->(year_month) {
+    return all if year_month.blank?
+    where("DATE_FORMAT(created_at, '%Y-%m') = ?", year_month) }
 end
