4e6cd24902b909d70f3103e027dc99bb5e3834f5
yuta20253 <magee.20253@gmail.com>
2025-05-24 01:55:04 +0900

    feat: 作り方のinputタグを追加するjavascript実装

--- rails/app/javascript/application.js
+++ rails/app/javascript/application.js
@@ -1,3 +1,45 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+document.addEventListener("turbo:load", function () {
+  const container = document.getElementById("instructions-container");
+  const addButton = document.getElementById("add-step-button");
+  const form = document.querySelector("form");
+  const hiddenInput = document.getElementById("compiled_instructions");
+  let stepCount = 1;
+
+  addButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    stepCount++;
+
+    const newDiv = document.createElement("div");
+      newDiv.className = "instruction-field mb-2";
+
+      const label = document.createElement("label");
+      label.innerText = `手順 ${stepCount}`;
+
+      const input = document.createElement("input");
+      input.type = "text";
+      input.name = "recipe[instructions][]";
+      input.className = "form-control";
+
+      newDiv.appendChild(label);
+      newDiv.appendChild(input);
+      container.appendChild(newDiv);
+  });
+
+  form.addEventListener("submit", function () {
+    const inputs = document.querySelectorAll('input[name="recipe[instructions][]"]');
+    const steps = [];
+
+    inputs.forEach((input, index) => {
+        const value = input.value.trim();
+        if (value !== "") {
+            steps.push(`${index + 1}. ${value}`);
+        }
+    });
+    hiddenInput.value = steps.join("\n");
+  })
+
+});
