76292c6bc4d94edc8cb7f71011bd7addf9cf7b33
yuta20253 <magee.20253@gmail.com>
2025-05-27 00:50:27 +0900

    add: Not Foundエラーハンドリング実装

---
+++ rails/app/controllers/concerns/error_handlers.rb
@@ -0,0 +1,17 @@
+module ErrorHandlers
+  extend ActiveSupport::Concern
+
+  included do
+    rescue_from StandardError, with: :rescue500
+    rescue_from ActiveRecord::RecordNotFound, with: :rescue404
+  end
+
+  private
+    def rescue404(e)
+      render "errors/not_found", status: 404
+    end
+
+    def rescue500(e)
+      render "errors/internal_server_error", status: 500
+    end
+end
